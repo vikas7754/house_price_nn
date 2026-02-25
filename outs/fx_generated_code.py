2ff9db7d2baaee11280d811bf25db5705f67b605
Vikas <vikas@zettabolt.com>
2026-02-25 17:19:53 +0530

    Build new model file with custom mul

--- outs/fx_generated_code.py
+++ outs/fx_generated_code.py
@@ -7,7 +7,9 @@ def forward(self, x):
     base_model_net_2 = getattr(self.base_model.net, "2")(base_model_net_1);  base_model_net_1 = None
     base_model_net_3 = getattr(self.base_model.net, "3")(base_model_net_2);  base_model_net_2 = None
     base_model_net_4 = getattr(self.base_model.net, "4")(base_model_net_3);  base_model_net_3 = None
+    scale_tensor = self.scale_tensor
+    custom_mul = torch.ops.my_mul_ops.custom_mul(base_model_net_4, scale_tensor);  base_model_net_4 = scale_tensor = None
     bias_tensor = self.bias_tensor
-    custom_add = torch.ops.my_ops.custom_add(base_model_net_4, bias_tensor);  base_model_net_4 = bias_tensor = None
+    custom_add = torch.ops.my_ops.custom_add(custom_mul, bias_tensor);  custom_mul = bias_tensor = None
     return custom_add
     
